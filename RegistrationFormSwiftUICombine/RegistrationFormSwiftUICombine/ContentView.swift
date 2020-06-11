71ea7144c45c5619e61d731065236dafe9505883
Vlad <vladpoberezhets123@gmail.com>
2020-06-11 17:49:55 +0300

    added registration UI

--- RegistrationFormSwiftUICombine/RegistrationFormSwiftUICombine/ContentView.swift
+++ RegistrationFormSwiftUICombine/RegistrationFormSwiftUICombine/ContentView.swift
@@ -9,8 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userName:String = ""
+    @State var email:String = ""
+    @State var password:String = ""
+    @State var confirmPassword:String = ""
+    private var heightTextFiled:CGFloat = 30
+    private var errorPadding:CGFloat = 1.3
+    private var buttonHeight:CGFloat = 13
+    private var buttonWidth:CGFloat = 29
+    private var paddingButton:CGFloat = 30
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader{geometry in
+            NavigationView{
+                VStack{
+                    VStack(alignment:.leading){
+                        Text("Organisation name").font(.callout).foregroundColor(Color.gray).padding(.leading)
+                        Divider().hidden()
+                    }
+                    VStack(alignment:.leading){
+                        Text("User name")
+                            .font(.subheadline)
+                            .foregroundColor(Color.green)
+                        TextField("Enter your user name", text: self.$userName).frame(height:self.heightTextFiled)
+                        Divider()
+                        Text("Error").font(.footnote).foregroundColor(Color.red).padding(.leading,geometry.size.width/1.3)
+                    }.padding([.leading,.trailing,.top])
+                    
+                    VStack(alignment:.leading){
+                        Text("Email")
+                            .font(.subheadline)
+                            .foregroundColor(Color.green)
+                        TextField("Enter your email", text: self.$email).frame(height:self.heightTextFiled)
+                        Divider()
+                        Text("Error").font(.footnote).foregroundColor(Color.red).padding(.leading,geometry.size.width/self.errorPadding)
+                    }.padding([.leading,.trailing])
+                    
+                    VStack(alignment:.leading){
+                        Text("Password")
+                            .font(.subheadline)
+                            .foregroundColor(Color.green)
+                        TextField("Enter your password", text: self.$password).frame(height:self.heightTextFiled)
+                        Divider()
+                        Text("Error").font(.footnote).foregroundColor(Color.red).padding(.leading,geometry.size.width/self.errorPadding)
+                    }.padding([.leading,.trailing])
+                    
+                    VStack(alignment:.leading){
+                        Text("Confirm password")
+                            .font(.subheadline)
+                            .foregroundColor(Color.green)
+                        TextField("Confirm your password", text: self.$confirmPassword).frame(height:self.heightTextFiled)
+                        Divider()
+                        Text("Error").font(.footnote).foregroundColor(Color.red).padding(.leading,geometry.size.width/self.errorPadding)
+                    }.padding([.leading,.trailing])
+                    
+                    Button(action: {
+                        print("sign up")
+                    }, label: {Text("Sign up").fontWeight(.bold).foregroundColor(.white).frame(width:geometry.size.width/self.errorPadding, height:geometry.size.height/self.buttonHeight)}).background(Color(.green)).cornerRadius(geometry.size.width/self.buttonWidth).padding(.top,self.paddingButton)
+                    Spacer()
+                }.navigationBarTitle("Registration")
+            }
+        }
     }
 }
 

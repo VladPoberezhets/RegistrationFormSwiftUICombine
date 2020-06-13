eb42a1282bb822c26a5e84da4b8400cb6af35a0c
Vlad <vladpoberezhets123@gmail.com>
2020-06-13 12:56:05 +0300

    Create logIn view

---
+++ RegistrationFormSwiftUICombine/RegistrationFormSwiftUICombine/LogIn.swift
@@ -0,0 +1,55 @@
+//
+//  LogIn.swift
+//  RegistrationFormSwiftUICombine
+//
+//  Created by Vlad on 13.06.2020.
+//  Copyright © 2020 Vlad. All rights reserved.
+//
+
+import SwiftUI
+
+struct LogIn: View {
+    @State var name = ""
+    
+    private var heightTextFiled:CGFloat = 30
+    private var errorPadding:CGFloat = 1.3
+    private var buttonHeight:CGFloat = 13
+    private var buttonWidth:CGFloat = 29
+    private var paddingButton:CGFloat = 30
+    
+    var body: some View {
+        GeometryReader{geometry in
+            NavigationView{
+            VStack{
+                VStack(alignment:.leading){
+                    Text("Corporation name").padding(.leading).font(.callout).foregroundColor(Color.gray)
+                    Divider().hidden()
+                }
+                VStack(alignment:.leading){
+                    Text("User name or email").font(.subheadline)
+                    .foregroundColor(Color.green)
+                    TextField("Enter user name or email", text: self.$name).frame(height:self.heightTextFiled)
+                }.padding([.leading, .trailing, .bottom]).padding(.top,100)
+                
+                VStack(alignment:.leading){
+                    Text("Password").font(.subheadline)
+                    .foregroundColor(Color.green)
+                    SecureField("Enter password", text: self.$name).frame(height:self.heightTextFiled)
+                }.padding([.leading, .trailing, .bottom])
+                
+                Button(action: {
+                    print("sign in")
+                }, label: {Text("Log in").fontWeight(.bold).foregroundColor(.white).frame(width:geometry.size.width/self.errorPadding, height:geometry.size.height/self.buttonHeight)}).background(Color(.green)).cornerRadius(geometry.size.width/self.buttonWidth).padding(.top,self.paddingButton)
+                Spacer()
+                .navigationBarTitle("Log in")
+            }
+            }
+        }
+    }
+}
+
+struct LogIn_Previews: PreviewProvider {
+    static var previews: some View {
+        LogIn()
+    }
+}
